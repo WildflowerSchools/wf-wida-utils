bd4f9525444e0be3bd9757542fd06cfd28f92e7e
Theodore Quinn <tcquinn@tcquinn.org>
2020-11-16 10:26:43 -0800

    Add WildflowerDatabasePandas object

--- wf_base_data/wf_database_pandas.py
+++ wf_base_data/wf_database_pandas.py
@@ -1,3 +1,4 @@
+from .database_pandas import DatabasePandas
 
 SCHEMA = {
     'student_ids': {
@@ -26,32 +27,6 @@ SCHEMA = {
     }
 }
 
-# class StudentIDDataTablePandas(DataTablePandas):
-#     def __init__(self):
-#         super().__init__(
-#             key_column_names = [
-#                 'tc_school_id',
-#                 'tc_student_id'
-#             ],
-#             value_column_names = [
-#                 'student_id'
-#             ]
-#         )
-#
-# class TransparentClassroomStudentDataTablePandas(DataTablePandas):
-#     def __init__(self):
-#         super().__init__(
-#             key_column_names = [
-#                 'tc_school_id',
-#                 'tc_student_id',
-#                 'pull_datetime'
-#             ],
-#             value_column_names = [
-#                 'student_first_name',
-#                 'student_last_name',
-#                 'student_birth_date',
-#                 'student_gender',
-#                 'student_ethnicity_list',
-#                 'student_dominant_language'
-#             ]
-#         )
+class WildflowerDatabasePandas(DatabasePandas):
+    def __init__(self):
+        super().__init__(schema=SCHEMA)
