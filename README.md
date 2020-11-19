be0fd0118a053913fbc741b0cb1699f06beaa3e8
Theodore Quinn <tcquinn@tcquinn.org>
2020-11-19 11:24:25 -0800

    Adjust schema and Google sheets pulling logic to match data pulled directly from TC

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 Python tools for working with Wildflower Schools base data
 
 ## Tasks
-* Add methods to pull data directly from Transparent Classroom
+* Figure out why `student_last_day` ends up being a mix of `NaT` and `NaN` for missing values
+* Add methods to pull all current Wildflower student IDs, all TC student IDs in history, all TC student IDs in mapping
+* Add school ID when pulling student data for multiple schools from Transparent Classroom
+* Add pull datetime when pulling student data from TC and writing to database
+* Add method for pulling student data directly from TC and writing to database
+* Make database structure an OrderedDict so user can specify order of data tables (e.g., for saving to Google sheets)
+* Add method for writing database to Google Sheets
+* Add method for writing database to local file(s)
 * Add tables for schools, classrooms, teachers
 * Generalize student ID generation to other objects (teachers, schools, hubs, etc.)
 * Use `__getitem__` so that user can pull individual data tables without referencing internals
--- wf_base_data/wf_database_pandas.py
+++ wf_base_data/wf_database_pandas.py
@@ -8,21 +8,40 @@ import logging
 logger = logging.getLogger(__name__)
 
 DATABASE_SCHEMA = {
-    'student_ids': OrderedDict([
-        ('tc_school_id', {'key': True, 'type': 'integer'}),
-        ('tc_student_id', {'key': True, 'type': 'integer'}),
-        ('student_id', {'type': 'integer'})
+    'students': OrderedDict([
+        ('student_id_wf', {'key': True, 'type': 'integer'})
     ]),
-    'transparent_classroom_student_data': OrderedDict([
-        ('tc_school_id', {'key': True, 'type': 'integer'}),
-        ('tc_student_id', {'key': True, 'type': 'integer'}),
+    'transparent_classroom_students': OrderedDict([
+        ('school_id_tc', {'key': True, 'type': 'integer'}),
+        ('student_id_tc', {'key': True, 'type': 'integer'}),
+        ('student_id_wf', {'type': 'integer'})
+    ]),
+    'transparent_classroom_student_data_history': OrderedDict([
+        ('school_id_tc', {'key': True, 'type': 'integer'}),
+        ('student_id_tc', {'key': True, 'type': 'integer'}),
         ('pull_datetime', {'key': True, 'type': 'datetime'}),
         ('student_first_name', {'type': 'string'}),
+        ('student_middle_name', {'type': 'string'}),
         ('student_last_name', {'type': 'string'}),
         ('student_birth_date', {'type': 'date'}),
         ('student_gender', {'type': 'string'}),
-        ('student_ethnicity_list', {'type': 'list'}),
-        ('student_dominant_language', {'type': 'string'})
+        ('student_ethnicity', {'type': 'list'}),
+        ('student_dominant_language', {'type': 'string'}),
+        ('student_household_income', {'type': 'string'}),
+        ('student_grade', {'type': 'string'}),
+        ('student_classroom_ids', {'type': 'list'}),
+        ('student_program', {'type': 'string'}),
+        ('student_hours_string', {'type': 'string'}),
+        ('student_id_tc_alt', {'type': 'string'}),
+        ('student_allergies', {'type': 'string'}),
+        ('student_parent_ids', {'type': 'list'}),
+        ('student_approved_adults_string', {'type': 'string'}),
+        ('student_emergency_contacts_string', {'type': 'string'}),
+        ('student_notes', {'type': 'string'}),
+        ('student_last_day', {'type': 'date'}),
+        ('student_exit_reason', {'type': 'string'}),
+        ('student_exit_survey_id', {'type': 'integer'}),
+        ('student_exit_notes', {'type': 'string'})
     ])
 }
 
@@ -51,19 +70,19 @@ class WildflowerDatabasePandas(DatabasePandas):
         self.add_transparent_classroom_student_records(records)
 
     def add_transparent_classroom_student_records(self, records):
-        self.data_tables['transparent_classroom_student_data'].create_records(records)
+        self.data_tables['transparent_classroom_student_data_history'].create_records(records)
         self.add_new_student_ids()
 
     def add_new_student_ids(self):
         logger.info('Adding Wildflower student IDs for new Transparent Classroom students')
         tc_student_ids = (
             self
-            .data_tables['transparent_classroom_student_data']
+            .data_tables['transparent_classroom_student_data_history']
             .index()
             .droplevel('pull_datetime')
             .drop_duplicates()
         )
-        new_tc_student_ids = tc_student_ids.difference(self.data_tables['student_ids'].index())
+        new_tc_student_ids = tc_student_ids.difference(self.data_tables['transparent_classroom_students'].index())
         num_new_tc_student_ids = len(new_tc_student_ids)
         if num_new_tc_student_ids == 0:
             logger.info('No new Transparent Classroom students found.')
@@ -71,17 +90,17 @@ class WildflowerDatabasePandas(DatabasePandas):
         logger.info('{} new Transparent Classroom students found. Generating Wildflower student IDs for them'.format(num_new_tc_student_ids))
         while True:
             student_id_records = pd.DataFrame(
-                {'student_id': [self.generate_student_id() for _ in new_tc_student_ids]},
+                {'student_id_wf': [self.generate_student_id() for _ in new_tc_student_ids]},
                 index=new_tc_student_ids
             )
             if (
-                not student_id_records['student_id'].duplicated().any() and
-                len(set(student_id_records['student_id']).intersection(set(self.data_tables['student_ids'].dataframe()['student_id']))) == 0
+                not student_id_records['student_id_wf'].duplicated().any() and
+                len(set(student_id_records['student_id_wf']).intersection(set(self.data_tables['transparent_classroom_students'].dataframe()['student_id_wf']))) == 0
             ):
                 break
             logger.info('Duplicates found among generated Wildflower student IDs. Regenerating.')
         logger.info('Adding new Wildflower student IDs to database.')
-        self.data_tables['student_ids'].create_records(student_id_records)
+        self.data_tables['transparent_classroom_students'].create_records(student_id_records)
 
     def generate_student_id(self):
         uuid_object = uuid.uuid4()
