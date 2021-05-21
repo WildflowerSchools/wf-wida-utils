9de27bbfa51c7a19583cf4eb200377baaaf87966
Theodore Quinn <tcquinn@tcquinn.org>
2021-05-20 21:11:30 -0700

    Implement ability to create FastBridge rosters

--- wf_core_data/__init__.py
+++ wf_core_data/__init__.py
@@ -2,4 +2,5 @@ from .google_sheets import *
 from .transparent_classroom import *
 from .rosters.master_roster import *
 from .rosters.nwea_roster import *
+from .rosters.fastbridge_roster import *
 from .utils import *
