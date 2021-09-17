a61444f6cf025b149acc42368f081301eb262cea
Theodore Quinn <tcquinn@tcquinn.org>
2021-09-16 17:42:49 -0700

    Implement initial Fastbridge results parsing

---
+++ wf_core_data/analysis/fastbridge_analysis.py
@@ -0,0 +1,146 @@
+import wf_core_data.utils
+import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
+
+TERMS = [
+    'Fall',
+    'Winter',
+    'Spring'
+]
+
+ASSESSMENTS = {
+    'Early Reading English': [
+        'Early Reading English',
+        'Concepts of Print',
+        'Decodable Words',
+        'Letter Names',
+        'Letter Sounds',
+        'Nonsense Words',
+        'Onset Sounds',
+        'Oral Repetition',
+        'Word Rhyming',
+        'Sentence Reading',
+        'Sight Words',
+        'Word Blending',
+        'Word Segmenting',
+        'CBMR-English'
+    ],
+    'Early Reading Spanish': [
+        'Early Reading Spanish',
+        'Concepts of Print',
+        'Decodable Words',
+        'Letter Names',
+        'Letter Sounds',
+        'Onset Sounds',
+        'Oral Repetition',
+        'Word Rhyming',
+        'Sentence Reading',
+        'Sight Words',
+        'Syllable Reading',
+        'Word Blending',
+        'Word Segmenting',
+        'CBMR-Spanish'
+    ],
+    'Early Math': [
+        'Early Math',
+        'Composing',
+        'Counting Objects',
+        'Decomposing ONE',
+        'Decomposing KG',
+        'Equal Partitioning',
+        'Match Quantity',
+        'Number Sequence ONE',
+        'Number Sequence KG',
+        'Numeral Identification ONE',
+        'Numeral Identification KG',
+        'Place Value',
+        'QuantityDiscrimination Least',
+        'Quantity Discrimination Most',
+        'Subitizing',
+        'Verbal Addition',
+        'Verbal Subtraction',
+        'Story Problems'
+    ]
+}
+
+METRICS = [
+    'Risk Level',
+    'Percentile at Nation',
+    'Final Date'
+]
+
+FIELD_NAMES_LIST = list()
+for test, subtests in ASSESSMENTS.items():
+    for term in TERMS:
+        for subtest in subtests:
+            for metric in METRICS:
+                FIELD_NAMES_LIST.append({
+                    'test': test,
+                    'term': term,
+                    'subtest': subtest,
+                    'metric': metric,
+                    'field_name': ' '.join([
+                        term,
+                        subtest,
+                        metric
+                    ])
+                })
+FIELD_NAMES = pd.DataFrame(FIELD_NAMES_LIST)
+FIELD_NAMES.set_index(['test', 'field_name'], inplace=True)
+
+def parse_fastbridge_results(
+    results
+):
+    parsed_results = (
+        pd.melt(
+            results,
+            id_vars=['Assessment', 'FAST ID'],
+            value_vars=set(FIELD_NAMES.index.get_level_values('field_name')).intersection(results.columns),
+            var_name='field_name',
+            value_name='value'
+        )
+        .rename(columns={
+            'Assessment': 'test',
+            'FAST ID': 'fast_id'
+        })
+        .dropna(subset=['value'])
+        .join(
+            FIELD_NAMES,
+            how='left',
+            on=['test', 'field_name']
+        )
+        .reindex(columns=[
+            'term',
+            'test',
+            'subtest',
+            'fast_id',
+            'metric',
+            'value'
+        ])
+        .pivot(
+            index=[
+                'term',
+                'test',
+                'subtest',
+                'fast_id'
+            ],
+            columns='metric',
+            values='value'
+        )
+        .reindex(columns=[
+            'Final Date',
+            'Percentile at Nation',
+            'Risk Level'
+        ])
+        .rename(columns={
+            'Final Date': 'test_date',
+            'Percentile at Nation': 'percentile',
+            'Risk Level': 'risk_level'
+        })
+        .sort_index()
+    )
+    parsed_results['test_date'] = parsed_results['test_date'].apply(wf_core_data.utils.to_date)
+    parsed_results['percentile'] = pd.to_numeric(parsed_results['percentile']).astype('float')
+    return parsed_results
