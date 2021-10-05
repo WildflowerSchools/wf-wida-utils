31411a8dd13d24de089d5baa37ac4c00b92d48a7
Theodore Quinn <tcquinn@tcquinn.org>
2021-10-05 11:47:36 -0700

    Define field and category names as constants

--- wf_core_data/analysis/nwea_analysis.py
+++ wf_core_data/analysis/nwea_analysis.py
@@ -4,10 +4,46 @@ import numpy as np
 import inflection
 import collections
 import itertools
+import functools
 import os
 import logging
 
 logger = logging.getLogger(__name__)
+
+TIME_FRAME_ID_VARIABLES_NWEA = [
+    'school_year',
+    'term'
+]
+
+STUDENT_ID_VARIABLES_NWEA = [
+    'legal_entity',
+    'student_id_nwea'
+]
+
+STUDENT_INFO_VARIABLES_NWEA = [
+    'first_name',
+    'last_name'
+]
+
+STUDENT_ASSIGNMENT_VARIABLES_NWEA = [
+    'school',
+    'teacher_last_first',
+    'classroom',
+    'grade'
+]
+
+ASSESSMENT_ID_VARIABLES_NWEA = [
+    'subject',
+    'course'
+]
+
+RESULTS_VARIABLES_NWEA = [
+    'test_date',
+    'rit_score',
+    'rit_score_sem',
+    'percentile',
+    'percentile_se'
+]
 
 TERMS_NWEA = (
     'Fall',
@@ -15,99 +51,20 @@ TERMS_NWEA = (
     'Spring'
 )
 
-# ASSESSMENTS = collections.OrderedDict((
-#     ('Early Reading English', (
-#         'Early Reading English',
-#         'Concepts of Print',
-#         'Decodable Words',
-#         'Letter Names',
-#         'Letter Sounds',
-#         'Nonsense Words',
-#         'Onset Sounds',
-#         'Oral Repetition',
-#         'Word Rhyming',
-#         'Sentence Reading',
-#         'Sight Words',
-#         'Word Blending',
-#         'Word Segmenting',
-#         'CBMR-English'
-#     )),
-#     ('Early Reading Spanish', (
-#         'Early Reading Spanish',
-#         'Concepts of Print Spanish',
-#         'Decodable Words Spanish',
-#         'Letter Names Spanish',
-#         'Letter Sounds Spanish',
-#         'Onset Sounds Spanish',
-#         'Oral Repetition Spanish',
-#         'Word Rhyming Spanish',
-#         'Sentence Reading Spanish',
-#         'Sight Word Spanishs',
-#         'Syllable Reading Spanish',
-#         'Word Blending Spanish',
-#         'Word Segmenting Spanish',
-#         'CBMR-Spanish'
-#     )),
-#     ('Early Math', (
-#         'Early Math',
-#         'Composing',
-#         'Counting Objects',
-#         'Decomposing ONE',
-#         'Decomposing KG',
-#         'Equal Partitioning',
-#         'Match Quantity',
-#         'Number Sequence ONE',
-#         'Number Sequence KG',
-#         'Numeral Identification ONE',
-#         'Numeral Identification KG',
-#         'Place Value',
-#         'QuantityDiscrimination Least',
-#         'Quantity Discrimination Most',
-#         'Subitizing',
-#         'Verbal Addition',
-#         'Verbal Subtraction',
-#         'Story Problems'
-#     ))
-# ))
+ASSESSMENTS_NWEA = collections.OrderedDict((
+    ('Language Arts', [
+        'Reading',
+        'Reading (Spanish)',
+        'Language Usage'
+    ]),
+    ('Mathematics', [
+        'Math K-12'
+    ])
+))
 
-# METRICS = (
-#     'Risk Level',
-#     'Percentile at Nation',
-#     'Final Date'
-# )
-#
-# RISK_LEVELS=(
-#     'highRisk',
-#     'someRisk',
-#     'lowRisk'
-# )
+SUBJECTS_NWEA = list(ASSESSMENTS_NWEA.keys())
 
-# FIELD_NAMES_LIST = list()
-# for test, subtests in ASSESSMENTS.items():
-#     for term in TERMS:
-#         for subtest in subtests:
-#             for metric in METRICS:
-#                 FIELD_NAMES_LIST.append({
-#                     'test': test,
-#                     'term': term,
-#                     'subtest': subtest,
-#                     'metric': metric,
-#                     'field_name': ' '.join([
-#                         term,
-#                         subtest,
-#                         metric
-#                     ])
-#                 })
-# FIELD_NAMES = pd.DataFrame(FIELD_NAMES_LIST)
-# FIELD_NAMES.set_index(['test', 'field_name'], inplace=True)
-#
-# TEMP = FIELD_NAMES.reset_index()
-# TEST_DATE_FIELD_NAMES = (
-#     TEMP
-#     .loc[TEMP['metric'] == 'Final Date', 'field_name']
-#     .tolist()
-# )
-#
+COURSES_NWEA=list(itertools.chain(*ASSESSMENTS_NWEA.values()))
 
 def fetch_results_local_directory_nwea(
     path,
@@ -190,33 +147,33 @@ def extract_test_events_nwea(
         categories=TERMS_NWEA,
         ordered=True
     )
+    test_events['subject'] = pd.Categorical(
+        test_events['subject'],
+        categories=SUBJECTS_NWEA,
+        ordered=True
+    )
+    test_events['course'] = pd.Categorical(
+        test_events['course'],
+        categories=COURSES_NWEA,
+        ordered=True
+    )
     test_events['test_date'] = test_events['test_date'].apply(wf_core_data.utils.to_date)
     test_events['rit_score'] = pd.to_numeric(test_events['rit_score']).astype('float')
     test_events['rit_score_sem'] = pd.to_numeric(test_events['rit_score_sem']).astype('float')
     test_events['percentile'] = pd.to_numeric(test_events['percentile']).astype('float')
     test_events['percentile_se'] = pd.to_numeric(test_events['percentile_se'].replace('<1', 0.5)).astype('float')
-    test_events = test_events.reindex(columns=[
-        'school_year',
-        'term',
-        'subject',
-        'course',
-        'legal_entity',
-        'student_id_nwea',
-        'test_date',
-        'rit_score',
-        'rit_score_sem',
-        'percentile',
-        'percentile_se'
-    ])
+    test_events = test_events.reindex(columns=
+        TIME_FRAME_ID_VARIABLES_NWEA +
+        ASSESSMENT_ID_VARIABLES_NWEA +
+        STUDENT_ID_VARIABLES_NWEA +
+        RESULTS_VARIABLES_NWEA
+    )
     test_events.set_index(
-        [
-            'school_year',
-            'term',
-            'subject',
-            'course',
-            'legal_entity',
-            'student_id_nwea'
-        ],
+        (
+            TIME_FRAME_ID_VARIABLES_NWEA +
+            ASSESSMENT_ID_VARIABLES_NWEA +
+            STUDENT_ID_VARIABLES_NWEA
+        ),
         inplace=True
     )
     test_events.sort_index(inplace=True)
@@ -239,28 +196,25 @@ def extract_student_info_nwea(
     student_info['school_year'] = student_info['term_school_year'].apply(lambda x: x.split(' ')[1])
     student_info = (
         student_info
-        .reindex(columns=[
-            'legal_entity',
-            'student_id_nwea',
-            'school_year',
-            'term',
-            'first_name',
-            'last_name'
-        ])
+        .reindex(columns=
+            STUDENT_ID_VARIABLES_NWEA +
+            TIME_FRAME_ID_VARIABLES_NWEA +
+            STUDENT_INFO_VARIABLES_NWEA
+        )
         .drop_duplicates()
     )
     student_info_changes = (
         student_info
-        .groupby(['legal_entity', 'student_id_nwea'])
-        .filter(lambda group: len(group.drop_duplicates(subset=['first_name', 'last_name'])) > 1)
+        .groupby(STUDENT_ID_VARIABLES_NWEA)
+        .filter(lambda group: len(group.drop_duplicates(subset=STUDENT_INFO_VARIABLES_NWEA)) > 1)
     )
     student_info = (
         student_info
-        .sort_values(['school_year', 'term'])
-        .drop(columns=['school_year', 'term'])
-        .groupby((['legal_entity', 'student_id_nwea']))
+        .sort_values(TIME_FRAME_ID_VARIABLES_NWEA)
+        .drop(columns=TIME_FRAME_ID_VARIABLES_NWEA)
+        .groupby(STUDENT_INFO_VARIABLES_NWEA)
         .tail(1)
-        .set_index(['legal_entity', 'student_id_nwea'])
+        .set_index(STUDENT_ID_VARIABLES_NWEA)
         .sort_index()
     )
     return student_info, student_info_changes
@@ -284,18 +238,16 @@ def extract_student_assignments_nwea(
     student_assignments['school_year'] = student_assignments['term_school_year'].apply(lambda x: x.split(' ')[1])
     student_assignments = (
         student_assignments
-        .reindex(columns=[
-            'legal_entity',
-            'student_id_nwea',
-            'school_year',
-            'term',
-            'school',
-            'teacher_last_first',
-            'classroom',
-            'grade'
-        ])
+        .reindex(columns=
+            STUDENT_ID_VARIABLES_NWEA +
+            TIME_FRAME_ID_VARIABLES_NWEA +
+            STUDENT_ASSIGNMENT_VARIABLES_NWEA
+        )
         .drop_duplicates()
-        .set_index(['legal_entity', 'student_id_nwea', 'school_year', 'term'])
+        .set_index(
+            STUDENT_ID_VARIABLES_NWEA +
+            TIME_FRAME_ID_VARIABLES_NWEA
+        )
         .sort_index()
     )
     return student_assignments
@@ -323,10 +275,10 @@ def summarize_by_student_nwea(
         .sort_values('test_date')
         .groupby(new_index_variables)
         .agg(
-            rit_score_starting_date=('test_date', lambda x: x.iloc[0]),
-            rit_score_ending_date=('test_date', lambda x: x.iloc[-1]),
-            starting_rit_score=('rit_score', lambda x: x.iloc[0]),
-            ending_rit_score=('rit_score', lambda x: x.iloc[-1]),
+            rit_score_starting_date=('test_date', lambda x: x.dropna().iloc[0]),
+            rit_score_ending_date=('test_date', lambda x: x.dropna().iloc[-1]),
+            starting_rit_score=('rit_score', lambda x: x.dropna().iloc[0]),
+            ending_rit_score=('rit_score', lambda x: x.dropna().iloc[-1]),
         )
     )
     percentiles = (
@@ -342,7 +294,7 @@ def summarize_by_student_nwea(
         )
     )
     students = (
-    students
+        students
         .join(
             rit_scores,
             how='left'
@@ -388,8 +340,17 @@ def summarize_by_student_nwea(
         ] +
         underlying_data_columns +
         [
-            'ending_percentile',
+            'rit_score_starting_date',
+            'rit_score_ending_date',
+            'rit_score_num_days',
+            'starting_rit_score',
+            'ending_rit_score',
             'rit_score_growth',
+            'percentile_starting_date',
+            'percentile_ending_date',
+            'percentile_num_days',
+            'starting_percentile',
+            'ending_percentile',
             'percentile_growth'
         ]
     )
